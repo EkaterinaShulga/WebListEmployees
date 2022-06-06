70425fa914984b431ac6dd007e9c02485b065eb6
EkaterinaShulga <shulga23@gmail.com>
2022-06-06 14:13:27 +0300

    первый коммит

---
+++ src/main/java/pro/sky/java/course2/weblistemployees/WebListEmployeesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.weblistemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebListEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebListEmployeesApplication.class, args);
+	}
+
+}
